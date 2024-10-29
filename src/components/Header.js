d38b4567d8318fcf60a27b9f764ff76eb84230ca
Arianna <164910808+aharri@users.noreply.github.com>
2024-10-29 18:17:40 -0500

    Header.js - styling, logo, info link added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {Card, Nav, Navbar} from 'react-bootstrap';
 //import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-
-
+import logo from '../images/cblogo.png';
 
 
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -11,18 +10,22 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header(props) {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">Commerce Bank</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/loan">Loan</Nav.Link>
-                <Nav.Link href="/customer">Customer</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+      <Navbar expand="lg" className="custom-navbar">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" style={{ width: '300px', height: 'auto' }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/loan">Loan</Nav.Link>
+            <Nav.Link href="/customer">Customer</Nav.Link>
+            <Nav.Link href="/info">Info</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
       );
     }
     
