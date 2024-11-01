65503d0108560dc1abf9d5e781634187cc28b774
dcornell03 <dlc89090@ucmo.edu>
2024-11-01 10:00:52 -0500

    Updated Customer.js
    I put it in a grid system and also linked the account information button to Gavin's info.js

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,49 +1,54 @@
 import React from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Table, Container, Row, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
+import { useNavigate } from 'react-router-dom';
 
-//<Image src="MyImages/commercebank.png" alt=""/>
-
- 
 function Customer() {
+  const navigate = useNavigate();
+
   return (
-    <div style={{margin: "0em 3em 0em 3em"}}>
-      <br></br>
-      <div style={{margin: "0em 0em 0em 6em"}}>
-      <Button variant="success">Back</Button>{' '}
-      </div>
-      <br></br>
-      <br></br>
-      <div style={{margin: "0em 0em 0em 57em"}}>
-      <Button variant="success">Account Information</Button>{' '}
-      </div>
-      <div style={{margin: "0em 6em 0em 6em"}}>
-      <h3 style={{color: "#006E52"}}>Your Loans</h3>
-        <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Amount Owed</th>
-          <th>Original Amount</th>
-          <th>Interest Rate</th>
-          <th>Pay-off Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>03/14/24</td>
-          <td>$7,894.65</td>
-          <td>$10,000.00</td>
-          <td>5.76%</td>
-          <td>08/14/26</td>
-        </tr>
-        
-      </tbody>
-    </Table>
-    </div>
-    </div>
+    <Container>
+      <Row className="my-3 align-items-center">
+        <Col className="text-start">
+          <Button variant="success">Back</Button>
+        </Col>
+        <Col className="text-center">
+          <Image src="MyImages/CommerceBankLogo.png" alt="Commerce Bank Logo" fluid />
+        </Col>
+        <Col className="text-end">
+          <Button variant="success" onClick={() => navigate('/info')}>
+            Account Information
+          </Button>
+        </Col>
+      </Row>
+
+      <Row className="my-3">
+        <Col>
+          <h3 style={{ color: "#006E52" }}>Your Loans</h3>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Amount Owed</th>
+                <th>Original Amount</th>
+                <th>Interest Rate</th>
+                <th>Pay-off Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>03/14/24</td>
+                <td>$7,894.65</td>
+                <td>$10,000.00</td>
+                <td>5.76%</td>
+                <td>08/14/26</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
 
 export default Customer;
