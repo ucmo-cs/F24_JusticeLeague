8b6d8fc79635a621a0d903c2ec1f831de73dae1c
OGPanther08 <159088470+OGPanther08@users.noreply.github.com>
2024-11-02 15:10:44 -0500

    Update AccountController.java

--- demo/controller/AccountController.java
+++ demo/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import com.example.demo.domain.Account;
 import com.example.demo.dto.AccountDto;
 import com.example.demo.dto.LoginResponseDto;
@@ -17,43 +16,41 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/accounts") // Base URL for account-related endpoints
 public class AccountController {
 
     private final AccountService accountService;
     private final AccountRepository accountRepository;
 
     @CrossOrigin
-    @PostMapping("/account")
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody AccountDto accountDto) {
-
         Account account = new ModelMapper().map(accountDto, Account.class);
         account.setCreated_at(new Timestamp(System.currentTimeMillis()));
-
         return new ResponseEntity<>(accountService.create(account), HttpStatus.CREATED);
-
     }
+
     @CrossOrigin
     @PostMapping("/user")
     public ResponseEntity<?> login(@RequestBody AccountDto accountDto) {
-
         System.out.println("user id : " + accountDto.getUserId());
         System.out.println("user password : " + accountDto.getPassword());
 
         Optional<Account> acct = accountRepository.findByUserId(accountDto.getUserId());
 
-        //check if user exists and if the passwords matches
+        // Check if user exists and if the passwords match
         if(acct.isPresent() && acct.get().getPassword().equals(accountDto.getPassword())) {
             System.out.println("Login Successful!");
             LoginResponseDto response = new LoginResponseDto("Login Successful!", acct.get().getUser_type());
             return ResponseEntity.ok(response);
-        }else{
+        } else {
             System.out.println("Login Failed!");
             return ResponseEntity.status(401).body("Invalid user Id or Password");
         }
     }
 
     @CrossOrigin
-    @GetMapping("/account/{userId}")
+    @GetMapping("/{userId}")
     public ResponseEntity<?> findByUserId(@PathVariable String userId) {
         Account account = accountService.findByUserId(userId);
         if (account != null) {
@@ -62,6 +59,4 @@ public class AccountController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
-
