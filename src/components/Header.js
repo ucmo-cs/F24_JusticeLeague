6e91ff63de0468e1a3572ea394692b97c01f5b01
beccarubak <161472394+beccarubak@users.noreply.github.com>
2024-11-06 14:11:49 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import {Card, Nav, Navbar} from 'react-bootstrap';
-//import { Link } from 'react-router-dom';
+import { Nav, Navbar} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import logo from '../images/cblogo.png';
 
-
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
-function Header(props) {
+function Header() {
     return (
       <Navbar expand="lg" className="custom-navbar">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" style={{ width: '300px', height: 'auto' }} />
+        <img src="MyImages/CommerceBankLogo.png" alt="logo" style={{ width: '300px', height: 'auto' }} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/loan">Loan</Nav.Link>
             <Nav.Link href="/customer">Customer</Nav.Link>
-            <Nav.Link href="/info">Info</Nav.Link>
+            <Nav.Link href="/info">Account</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -28,6 +22,5 @@ function Header(props) {
     
       );
     }
-    
     
     export default Header;
