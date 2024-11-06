ad411cc5fcf6556e070450f195cba111240ed7e6
beccarubak <161472394+beccarubak@users.noreply.github.com>
2024-11-06 14:03:20 -0600

    Update AccountDto.java

--- demo/dto/AccountDto.java
+++ demo/dto/AccountDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class AccountDto {
 
         private String userId;
+        private String firstName;
+        private String lastName;
         private String userName;
         private String password;
         private String email;
