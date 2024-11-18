ae521d1825300bb3baaf3344d2c31090a69e760a
beccarubak <161472394+beccarubak@users.noreply.github.com>
2024-11-18 11:06:12 -0600

    Update LoanRepository.java

--- demo/repository/LoanRepository.java
+++ demo/repository/LoanRepository.java
@@ -2,10 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
     Optional<Loan> findById(Long loanId);
+
+    @Query("SELECT l FROM Loan l WHERE l.user_account.userId = :userId")
+    List<Loan> findLoansByUserId(@Param("userId") String userId);
 }
