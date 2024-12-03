ec51c59ad50a59f14e7f2d867e09f728938efa0d
beccarubak <161472394+beccarubak@users.noreply.github.com>
2024-12-03 16:50:41 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const styles = {
+    img: {
+      cursor: 'pointer',
+      width: '300px',
+      hieght: 'auto'
+    }
+  }
     return (
       <Navbar expand="lg" className="custom-navbar">
       <Container>
         <Navbar.Brand>
-        <img src="MyImages/CommerceBankLogo.png" alt="logo" style={{ width: '300px', height: 'auto' }} />
+        <img src="MyImages/CommerceBankLogo.png" alt="logo" style={styles.img} 
+        onClick={()=>navigate('/')}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/loan">Loan</Nav.Link>
-            <Nav.Link href="/customer">Customer</Nav.Link>
-            <Nav.Link href="/info">Account</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
