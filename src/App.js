669036c7fdca68c449fac9b4eabe7d26a4342cba
beccarubak <161472394+beccarubak@users.noreply.github.com>
2024-12-05 12:47:09 -0600

    Final Version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import Header from './components/Header';
-import Header2 from './components/header2';
-import { Route, Routes, useLocation } from 'react-router-dom';
+
+import { Route, Routes} from 'react-router-dom';
+
 import Customer from './pages/Customer';
 import SaveLoan from './pages/SaveLoan';
 import Loan_bk from './pages/Loan_bk';
 import LoginForm from './pages/LoginForm';
 import LoanInfo from './pages/loan_info';
+import Info from './pages/Info';
 
 function App() {
-  const location = useLocation();
-
-  // Determine which header to display based on the current route
-  const renderHeader = () => {
-    if (location.pathname === "/") return null; // No header for LoginForm
-    if (["/loanForm", "/loan", "/loanInfo/:accountId"].includes(location.pathname)) return <Header />;
-    if (location.pathname === "/customer") return <Header2 />;
-  };
+  
+ 
   return (
       <div>
-      {renderHeader()}
+        <Header />
         <Routes>
           <Route path="/" exact={true} element={<LoginForm />} />
           <Route path="/loanForm" exact={true} element={<SaveLoan />} />  
           <Route path="/loan" exact={true} element={<Loan_bk />} />
-          <Route path="/customer" exact={true} element={<Customer />} />
+          <Route path="/customer/:userId" exact={true} element={<Customer />} />
           <Route path="/loanInfo/:accountId" element={<LoanInfo />} /> {/* Use accountId */}
+          <Route path='/info/:userId' exact={true} element={<Info/>}/>
         </Routes>
       </div>
   );
